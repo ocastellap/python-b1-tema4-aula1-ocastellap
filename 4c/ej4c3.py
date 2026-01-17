99adee064d7f58f6f0d46ea45d5ed922c2d3db2e
ocastellap <ocastellap@uoc.edu>
2026-01-17 19:53:03 +0100

    Update ej4c3.py

--- 4c/ej4c3.py
+++ 4c/ej4c3.py
@@ -84,20 +84,25 @@ class Shape:
         return self.sides
     
 # Corret and overwrite class Triangle(Shape) here
-class Triangle():
-    def __init__(self, sides, base, height):        
-        pass
+class Triangle(Shape):
+    def __init__(self, sides, base, height):
+        super().__init__(sides)
+        self.base = base
+        self.height = height
 
     def get_area(self):
-        pass
+        return (self.base + self.height) / 2
 
 # Corret and overwrite class Rectangle(Shape) here
 class Rectangle(Shape):
-    def __init__(self, sides, length, width):        
-        pass
-
+    def __init__(self, sides, length, width):  
+        super().__init__(sides)
+        self.length = length
+        self.width = width
+        
     def get_area(self):
-        pass
+        return self.length * self.width
+        
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
